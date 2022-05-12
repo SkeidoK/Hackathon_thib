0c3a51528ff31dcc8fba1c9fe3b3bccc40ac43d1
codelande <corentin-delande_student2022@wilder.school>
2022-05-12 23:07:19 +0200

    voila

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -10,12 +10,17 @@ class SearchController extends AbstractController
     public function search()
     {
         $chercheur = new SearchManager();
-        $recherche = $chercheur->search($_GET['ville']);
+        $ville = $_GET['ville'];
+        $recherche = $chercheur->search($ville);
+        $ville = $recherche['features']['0']['properties']['city'];
 
         $x = (string)$recherche['features']['0']['geometry']['coordinates']['0'];
         $y = (string)$recherche['features']['0']['geometry']['coordinates']['1'];
 
-        return $this->twig->render('Home/index.html.twig', ['x' => $x, 'y' =>$y]);
+        $bornes = $chercheur->bornes($x, $y);
+
+
+        return $this->twig->render('Home/index.html.twig', ['x' => $x, 'y' =>$y, 'bornes' => $bornes, 'ville' => $ville]);
 
     }
 }
--- src/Model/SearchManager.php
+++ src/Model/SearchManager.php
@@ -23,8 +23,21 @@ class SearchManager extends AbstractManager
     }
     public function bornes($x, $y)
     {
-        $x;
-        $y;
+        $client = HttpClient::create();
+        
+        $response = $client->request('GET', 'https://api.openchargemap.io/v3/poi?maxresults=500&countrycode=fr&latitude=' . (string)$y . '&longitude=' . (string)$x . '&distance=50&distanceunit=km&compact=true&key=%20da842c89-f490-49ef-993c-f2edbdb63496%20%27');
+
+        $statusCode = $response->getStatusCode();
+
+        if ($statusCode === 200) {
+            $bornes = [];
+            $bornes = $response->getContent();
+            $bornes = $response->toArray();
+            $bornes = count($bornes);
+            $result = $bornes * 620;
+            $trucs = [$result, $bornes];
+            return $trucs;
+        }
     }
 
 }
